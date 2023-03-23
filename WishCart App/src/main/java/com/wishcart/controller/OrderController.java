633e80e5d78970a992cf5074829161b3160bba36
Adnan <adnan.hussain.136660@gmail.com>
2023-03-23 15:39:08 +0530

    updated

--- WishCart App/src/main/java/com/wishcart/controller/OrderController.java
+++ WishCart App/src/main/java/com/wishcart/controller/OrderController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wishcart.dto.CardDto;
@@ -15,19 +14,21 @@ import com.wishcart.service.OrderService;
 
 @RestController
 public class OrderController {
-	
+
 	@Autowired
 	private OrderService orderservice;
-	
+
 	@PostMapping("/placeorder/{authkey}")
-	public ResponseEntity<Order> placeOrderHandler(@RequestBody CardDto carddto, @PathVariable("authkey") String authkey){
+	public ResponseEntity<Order> placeOrderHandler(@RequestBody CardDto carddto,
+			@PathVariable("authkey") String authkey) {
 		return new ResponseEntity<Order>(orderservice.placeOrder(carddto, authkey), HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/placeorderbyproductid/{productid}/{authkey}")
-	public ResponseEntity<Order> placeOrderByProductIdHandler(@RequestBody CardDto carddto, @PathVariable("productid") Integer productid, @PathVariable("authkey") String authkey){
-		return new ResponseEntity<Order>(
-				orderservice.placeOrderByProductId(carddto, productid, authkey), HttpStatus.OK);
+	public ResponseEntity<Order> placeOrderByProductIdHandler(@RequestBody CardDto carddto,
+			@PathVariable("productid") Integer productid, @PathVariable("authkey") String authkey) {
+		return new ResponseEntity<Order>(orderservice.placeOrderByProductId(carddto, productid, authkey),
+				HttpStatus.OK);
 	}
 
 }
