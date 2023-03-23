654e641047909ee0365b5865c1ad9f7f5a8c2c5a
Adnan <adnan.hussain.136660@gmail.com>
2023-03-23 16:39:51 +0530

    updated

--- WishCart App/src/main/java/com/wishcart/WishCartApplication.java
+++ WishCart App/src/main/java/com/wishcart/WishCartApplication.java
@@ -2,8 +2,10 @@ package com.wishcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(allowedHeaders = "*")
 public class WishCartApplication {
 
 	public static void main(String[] args) {
